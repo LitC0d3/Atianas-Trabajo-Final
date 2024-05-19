2dfc377c028da7587b11ae0ac17be6f60ff415c4
LitCode <Posho19012005@gmail.com>
2024-05-19 14:40:47 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoTest/Logica/Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTest.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source=.;Initial Catalog=DB_CARRITO;Integrated Security=True";
+    }
+}
