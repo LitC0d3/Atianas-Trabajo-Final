abd7771c0e29e887a6b7b2b668518cb8b616d708
MiguelAlejo24 <fernandoalejo68@gmail.com>
2024-05-30 17:10:16 -0500

    FORMULARIO (LOGIN) MEJORAR CSS

--- ProyectoTest/Logica/Conexion.cs
+++ ProyectoTest/Logica/Conexion.cs
@@ -2,6 +2,14 @@
 {
     public class Conexion
     {
-        public static string CN = "Data Source=DESKTOP-2VJHG78\\SQLEXPRESS;Initial Catalog=DB_ATIANAS;Integrated Security=True";
+        public static string CN = "Data Source=DESKTOP-8O5D36K\\SQLEXPRESS;Initial Catalog=DB_ATIANAS;Integrated Security=True";
     }
 }
+
+
+
+//Miguel Conexion bd:
+// DESKTOP - 8O5D36K\SQLEXPRESS
+
+// Aldair Conexion bd:
+//public static string CN = "Data Source=DESKTOP-2VJHG78\\SQLEXPRESS;Initial Catalog=DB_ATIANAS;Integrated Security=True";
